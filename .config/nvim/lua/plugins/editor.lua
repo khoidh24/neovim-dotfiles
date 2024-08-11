8a5c979444d5e608481754482cdac23a6bfc6024
khoidh24 <65653624+khoidh24@users.noreply.github.com>
2024-08-12 01:10:50 +0700

    Update editor.lua

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -48,13 +48,60 @@ return {
   },
   -- AUTOPAIRS
   {
-    {
-      "windwp/nvim-autopairs",
-      event = "InsertEnter",
-      config = true,
-      -- use opts = {} for passing setup options
-      -- this is equalent to setup({}) function
-    },
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      local npairs = require("nvim-autopairs")
+      local Rule = require("nvim-autopairs.rule")
+
+      npairs.setup({
+        check_ts = true,
+        ts_config = {
+          jsx = { "javascript", "typescript.tsx" },
+          tsx = { "typescript", "typescript.tsx" },
+        },
+      })
+
+      -- Add spaces between parentheses
+      local brackets = { { "(", ")" }, { "[", "]" }, { "{", "}" } }
+      npairs.add_rules({
+        Rule(" ", " "):with_pair(function(opts)
+          local pair = opts.line:sub(opts.col - 1, opts.col)
+          return vim.tbl_contains({
+            brackets[1][1] .. brackets[1][2],
+            brackets[2][1] .. brackets[2][2],
+            brackets[3][1] .. brackets[3][2],
+          }, pair)
+        end),
+      })
+
+      -- Move past commas and semicolons
+      for _, punct in pairs({ ",", ";" }) do
+        npairs.add_rules({
+          Rule("", punct)
+            :with_move(function(opts)
+              return opts.char == punct
+            end)
+            :with_pair(function()
+              return false
+            end)
+            :with_del(function()
+              return false
+            end)
+            :with_cr(function()
+              return false
+            end)
+            :use_key(punct),
+        })
+      end
+
+      -- Add angle brackets specifically for JSX/TSX
+      npairs.add_rules({
+        Rule("<", ">"):with_pair(function(opts)
+          return opts.filetype == "jsx" or opts.filetype == "tsx"
+        end),
+      })
+    end,
   },
   {
     "stevearc/dressing.nvim",
