e84698e73d5fa9a3f0fd4a893d4f3b29c07eeaad
khoidh24 <65653624+khoidh24@users.noreply.github.com>
2024-08-12 01:10:24 +0700

    Update colorscheme.lua

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,32 +1,10 @@
 return {
-  {
-    "sainnhe/sonokai",
-    priority = 1000,
-    config = function()
-      vim.g.sonokai_transparent_background = "1"
-      vim.g.sonokai_enable_italic = "1"
-      vim.g.sonokai_style = "atlantis"
-      vim.g.sonokai_diagnostic_text_highlight = "1"
-      vim.g.sonokai_better_performance = "1"
-      -- vim.cmd.colorscheme("sonokai")
-    end,
-  },
-  {
-    "Everblush/nvim",
-    name = "everblush",
-    config = function()
-      require("everblush").setup({
-        transparent_background = true,
-      })
-      vim.cmd("colorscheme everblush")
-    end,
-  },
-  {
-    "craftzdog/solarized-osaka.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- vim.cmd.colorscheme("solarized-osaka")
-    end,
-  },
+  "navarasu/onedark.nvim",
+  config = function()
+    require("onedark").setup({
+      style = "deep",
+      transparent = true,
+    })
+    require("onedark").load()
+  end,
 }
